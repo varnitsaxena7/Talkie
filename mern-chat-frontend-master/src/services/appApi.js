3ee60306d6d8d2933cd3d2e99088d32dcc9a0b46
varnitsaxena7 <saxenavarnit.code7@gmail.com>
2024-03-08 18:45:12 +0530

    Fix api endpoints

--- mern-chat-frontend-master/src/services/appApi.js
+++ mern-chat-frontend-master/src/services/appApi.js
@@ -2,10 +2,12 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // define a service user a base URL
 
+const SERVER = process.env.NODE_ENV === 'development' ? 'http://localhost:5001' : 'https://talkie-3.onrender.com';
+
 const appApi = createApi({
     reducerPath: "appApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: process.env.SERVER_URL,
+        baseUrl: SERVER,
     }),
 
     endpoints: (builder) => ({
