866e92acd813575ffff75f10c818dbc2099b877d
varnitsaxena7 <118539746+varnitsaxena7@users.noreply.github.com>
2024-03-13 13:25:07 +0530

    Update Signup.js

--- mern-chat-frontend-master/src/pages/Signup.js
+++ mern-chat-frontend-master/src/pages/Signup.js
@@ -29,7 +29,7 @@ function Signup() {
     async function uploadImage() {
         const data = new FormData();
         data.append("file", image);
-        data.append("upload_preset", "your-preset-here");
+        data.append("upload_preset", "ptgwdfiv");
         try {
             setUploadingImg(true);
             let res = await fetch("https://api.cloudinary.com/v1_1/do1bktvjm/image/upload", {
