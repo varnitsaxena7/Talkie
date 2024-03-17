5ae6e516bf01d3a2bf5ccbed98286a497c550b0f
varnitsaxena7 <saxenavarnit.code7@gmail.com>
2024-03-18 02:45:19 +0530

    features corrected

--- mern-chat-frontend-master/src/components/Sidebar.js
+++ mern-chat-frontend-master/src/components/Sidebar.js
@@ -66,14 +66,19 @@ function Sidebar() {
     }
     return (
         <>
-            <h2>Available rooms</h2>
-            <ListGroup>
-                {rooms.map((room, idx) => (
-                    <ListGroup.Item key={idx} onClick={() => joinRoom(room)} active={room == currentRoom} style={{ cursor: "pointer", display: "flex", justifyContent: "space-between" }}>
-                        {room} {currentRoom !== room && <span className="badge rounded-pill bg-primary">{user.newMessages[room]}</span>}
-                    </ListGroup.Item>
-                ))}
-            </ListGroup>
+          <h2>Available rooms</h2>
+                    <ListGroup>
+                        {rooms.map((room, idx) => (
+                            <ListGroup.Item 
+                                key={idx} 
+                                onClick={() => joinRoom(room)} 
+                                active={room == currentRoom} 
+                                style={{ cursor: "pointer", display: "flex", justifyContent: "space-between" }}
+                            >
+                                {room} {currentRoom !== room && <span className="badge rounded-pill bg-primary">{user.newMessages[room]}</span>}
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
             <h2>Members</h2>
             {members.map((member) => (
                 <ListGroup.Item key={member.id} style={{ cursor: "pointer" }} active={privateMemberMsg?._id == member?._id} onClick={() => handlePrivateMemberMsg(member)} disabled={member._id === user._id}>
