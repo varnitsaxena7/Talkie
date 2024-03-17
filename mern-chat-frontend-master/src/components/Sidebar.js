b60beaf1917c926685ea89c86bb2635c95674c16
varnitsaxena7 <saxenavarnit.code7@gmail.com>
2024-03-18 02:10:56 +0530

    Features Updated

--- mern-chat-frontend-master/src/components/Sidebar.js
+++ mern-chat-frontend-master/src/components/Sidebar.js
@@ -30,7 +30,7 @@ function Sidebar() {
 
     useEffect(() => {
         if (user) {
-            setCurrentRoom("general");
+            setCurrentRoom("Tech Talk");
             getRooms();
             socket.emit("join-room", "general");
             socket.emit("new-user");
