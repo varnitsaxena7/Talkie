01d8ff65a3ed0d98a37677ed3d1845649aa192fe
varnitsaxena7 <118539746+varnitsaxena7@users.noreply.github.com>
2024-03-18 12:54:53 +0530

    Update appContext.js

--- mern-chat-frontend-master/src/context/appContext.js
+++ mern-chat-frontend-master/src/context/appContext.js
@@ -1,10 +1,6 @@
 import { io } from "socket.io-client";
 import React from "react";
-let SOCKET_URL = 'http://localhost:5001';
-
-if (process.env.NODE_ENV !== 'development') {
-    SOCKET_URL = 'https://talkie-3.onrender.com';
-}
+let SOCKET_URL = 'https://talkie-3.onrender.com';
 
 export const socket = io(SOCKET_URL);
 // app context
